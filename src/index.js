24b4a49d9ea56a26ce3e59fe24b5c3c85118232b
Shalini-Barai <115135802+Shalini-Barai@users.noreply.github.com>
2023-01-29 20:33:16 +0530

    render1

--- src/index.js
+++ src/index.js
@@ -2,13 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-function App() {
-  return (
-    <div>
-      <p>I am learning React. My life is getting better.</p>
-    </div>
-  );
-}
+
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
